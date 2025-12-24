0646c6bd09c1be8898456c0eb70709c3e418801f
Thiago <thiagosm2006@gmail.com>
2025-12-23 22:48:49 -0300

    criar método para leitura do questionário

--- src/utils/FileUtil.java
+++ src/utils/FileUtil.java
@@ -5,33 +5,22 @@ import model.services.CadastrarPetService;
 
 import java.io.*;
 
-import static java.lang.IO.print;
-
 public class FileUtil {
 
-    private final CadastrarPetService cadastrarPetService;
-
-    public FileUtil(CadastrarPetService cadastrarPetService){
-        this.cadastrarPetService = cadastrarPetService;
-    }
-    public File criarArquivo(String nomeArquivo){
-        File file = new File(nomeArquivo);
-        return file;
+    public static File criarArquivo(String nomeArquivo){
+        return new File(nomeArquivo);
     }
 
-    public void lerFormulario(File file){
-        try {
-            FileReader fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr);
+    public static void lerFormulario(File file){
+        try{
+            BufferedReader br =  new BufferedReader(new FileReader(file));
             String linha;
-            for(int i=0;i<=13;i++){
-                while((linha = br.readLine()) != null){
-                    print(linha);
-                }
+
+            while(((linha = br.readLine()) != null)){
+                System.out.println(linha);
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+
+        }catch(IOException e){
             throw new RuntimeException(e);
         }
     }
