9cd9d44a30874c6552a987f6868f8e431d937519
Thiago <thiagosm2006@gmail.com>
2025-12-23 23:06:09 -0300

    chore/feat: adicionar classe pet

--- src/model/entities/Pet.java
+++ src/model/entities/Pet.java
@@ -1,4 +1,25 @@
 package model.entities;
 
+import model.enums.Sexo;
+import model.enums.TipoPet;
+
 public class Pet {
+
+    private String nomeCompleto;
+    private TipoPet tipoPet;
+    private Sexo sexo;
+    private String endereco;
+    private Integer idade;
+    private Double peso;
+    private String raca;
+
+    public Pet(String nomeCompleto, TipoPet tipoPet, Sexo sexo, String endereco, Integer idade, Double peso, String raca) {
+        this.nomeCompleto = nomeCompleto;
+        this.tipoPet = tipoPet;
+        this.sexo = sexo;
+        this.endereco = endereco;
+        this.idade = idade;
+        this.peso = peso;
+        this.raca = raca;
+    }
 }
