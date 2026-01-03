63e51d67ee1097acb3a68b75a59323f0fe85e3a7
Thiago Santos <thiagosm2006@gmail.com>
2026-01-03 00:16:56 -0300

    feat: criar enum para tipo do Pet

--- src/model/enums/TipoPet.java
+++ src/model/enums/TipoPet.java
@@ -1,4 +1,13 @@
 package model.enums;
 
 public enum TipoPet {
+
+    GATO("gato", 1),
+    CACHORRO("cachorro", 2);
+
+    public final int VALOR;
+
+    TipoPet(String nome, int VALOR){
+        this.VALOR = VALOR;
+    }
 }
