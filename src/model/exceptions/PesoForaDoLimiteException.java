bd7d74cfa5a690d4290340d6e7368f7e4addd85d
Thiago Santos <thiagosm2006@gmail.com>
2026-01-06 19:21:22 -0300

    feat(chore): adicionar exceção para peso fora do limite

---
+++ src/model/exceptions/PesoForaDoLimiteException.java
@@ -0,0 +1,7 @@
+package model.exceptions;
+
+public class PesoForaDoLimiteException extends RuntimeException {
+    public PesoForaDoLimiteException(String message) {
+        super(message);
+    }
+}
