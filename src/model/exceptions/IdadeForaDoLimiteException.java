490d6f1e51238e2f4db9c2a4e57eede96001301f
Thiago Santos <thiagosm2006@gmail.com>
2026-01-06 19:05:01 -0300

    feat(chore): criar exceção para idade fora do limite permitido

---
+++ src/model/exceptions/IdadeForaDoLimiteException.java
@@ -0,0 +1,7 @@
+package model.exceptions;
+
+public class IdadeForaDoLimiteException extends RuntimeException {
+    public IdadeForaDoLimiteException(String message) {
+        super(message);
+    }
+}
