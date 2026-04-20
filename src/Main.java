7523daf8fdadd26dc047fe146dbabf241daf5ce3
Thiago Santos <thiagosm2006@gmail.com>
2026-04-20 19:11:45 -0300

    feat: adicionar print

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
         MenuController menuController = new MenuController(scanner);
         menuController.iniciarMenu();
-        scanner.close();
+        System.out.println(
+            "Corinthians!!"
+        );
     }
 
